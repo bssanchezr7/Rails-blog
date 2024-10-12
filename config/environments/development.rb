e35c3ad5ecccb4bc51d045b28e826227c08e59af
Brandon Sanchez Ramos <86493207+bssanchezr7@users.noreply.github.com>
2024-10-12 21:38:33 +0000

    Build a Blog with Rails Part 10: Authentication with Devise (hasta el tiempo 10:30)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,4 +81,5 @@ Rails.application.configure do
   config.hosts << "localhost:3000"
 
   config.action_cable.allowed_request_origins = ["https://#{pf_host}", "http://localhost:3000"]
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
