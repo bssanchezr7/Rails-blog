989953508cf1452b675af9283467c5be7dc6cb69
Brandon Sanchez Ramos <86493207+bssanchezr7@users.noreply.github.com>
2024-10-29 03:13:15 +0000

    Build a Blog with Rails Part 13: Adding Scheduled Posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,12 +2,10 @@ class BlogPostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, only: [:edit, :update, :show, :destory]
   def index 
-    @blog_posts = BlogPost.all
+    @blog_posts = user_signed_in? ? BlogPost.all.sorted : BlogPost.published.sorted
   end
 
   def show
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
   end
 
   def new
@@ -42,10 +40,12 @@ class BlogPostsController < ApplicationController
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
   end
 end
